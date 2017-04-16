69cc4296bf40860f790c283ba66bfbddd2ef17bb
nmcox <nmcox@oakland.edu>
2017-04-16 13:05:20 -0400

    commented out

--- src/gui/CalendarActivity.java
+++ src/gui/CalendarActivity.java
@@ -1,59 +1,59 @@
-package gui;
+//package gui;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import javax.swing.border.EmptyBorder;
 
-import com.toedter.calendar.JDateChooser;
+//import com.toedter.calendar.JDateChooser;
 
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.CardLayout;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPasswordField;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
+//import javax.swing.JTextField;
+//import javax.swing.JButton;
+//import java.awt.CardLayout;
+//import javax.swing.JMenuBar;
+//import javax.swing.JMenuItem;
+//import javax.swing.JPasswordField;
+//import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import javax.swing.JLabel;
+//import java.awt.Font;
+//import javax.swing.SwingConstants;
 
 
 
-public class CalendarActivity extends JFrame {
+//public class CalendarActivity extends JFrame {
 	
-	private JPanel Cards;
-	private JPanel Calendar;
-	private CardLayout layout;
+	//private JPanel Cards;
+	//private JPanel Calendar;
+	//private CardLayout layout;
 	
-public CalendarActivity(JPanel newCards, CardLayout newLayout){
+//public CalendarActivity(JPanel newCards, CardLayout newLayout){
 		
-		Cards = newCards;
-		layout = newLayout;
-	}
+		//Cards = newCards;
+		//layout = newLayout;
+	//}
 	
-	public void startActivity(){
+	//public void startActivity(){
 		
-		Calendar = new JPanel();
-		Cards.add(Calendar, "calendar");
-		Calendar.setLayout(null);
+		//Calendar = new JPanel();
+		//Cards.add(Calendar, "calendar");
+		//Calendar.setLayout(null);
 		
 		
 		//date chooser
-		JDateChooser bills = new JDateChooser();
-		bills.setBounds(193, 21, 147, 32);
-		Calendar.add(bills);
+		//JDateChooser bills = new JDateChooser();
+		//bills.setBounds(193, 21, 147, 32);
+		//Calendar.add(bills);
 		//select date label
-		JLabel lblSelectADate = new JLabel("Select A Date:");
-		lblSelectADate.setBounds(45, 21, 195, 26);
-		Calendar.add(lblSelectADate);
+		//JLabel lblSelectADate = new JLabel("Select A Date:");
+		//lblSelectADate.setBounds(45, 21, 195, 26);
+		//Calendar.add(lblSelectADate);
 		//date submit button
-		JButton btnSubmitDate = new JButton("Submit Date");
-		btnSubmitDate.setBounds(355, 18, 152, 35);
-		Calendar.add(btnSubmitDate);
+		//JButton btnSubmitDate = new JButton("Submit Date");
+		//btnSubmitDate.setBounds(355, 18, 152, 35);
+		//Calendar.add(btnSubmitDate);
 		
-		layout.show(Cards, "calendar");
-	}
+		//layout.show(Cards, "calendar");
+	//}
 	
 	
-}
+//}
